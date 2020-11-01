00c20f5ef2de394592abff6f35c537d810db87a0
p-koskey <patiencekoskey@gmail.com>
2020-11-01 16:46:17 +0300

    update login and signup template

--- hood/views.py
+++ hood/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .forms import SignupForm
 # Create your views here.
-
+@login_required(login_url='login')
 def index(request):
      return render(request, 'index.html')
 
