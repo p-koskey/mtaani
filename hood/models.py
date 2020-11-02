be5ede7bb39d5ddf443ce5f6456c1afdbda2cd27
p-koskey <patiencekoskey@gmail.com>
2020-11-02 12:21:20 +0300

    remove bio column from Profile model

--- hood/models.py
+++ hood/models.py
@@ -9,7 +9,6 @@ from cloudinary.models import CloudinaryField
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=80, blank=True)
-    bio = models.TextField(max_length=254, blank=True)
     location = models.CharField(max_length=50, blank=True, null=True)
     profile_picture = CloudinaryField('image')
     neighbourhood = models.ForeignKey('Neighbourhood', on_delete=models.SET_NULL, null=True, related_name='occupant', blank=True)
