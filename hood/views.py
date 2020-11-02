cfe18acd8e1712a013d089f9da5097d8910de01b
p-koskey <patiencekoskey@gmail.com>
2020-11-02 12:23:09 +0300

    create function to show and edit profile

--- hood/views.py
+++ hood/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http  import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate, logout
-from .forms import SignupForm
+from .forms import SignupForm,UpdateUserForm,UpdateProfileForm
+from .models import Profile, User
 # Create your views here.
 @login_required(login_url='login')
 def index(request):
@@ -22,3 +23,25 @@ def register(request):
     else:
         form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form}) 
+
+
+@login_required(login_url='login')
+def profile(request, username):
+    return render(request, 'profile.html')
+
+
+@login_required(login_url='login')
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.username)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateProfileForm(instance=request.user.profile)
+    
+    return render(request, 'editprofile.html', {'user_form': user_form, 'prof_form': prof_form})
