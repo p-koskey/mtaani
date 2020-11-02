a94c77ee50d0226f3ab3166ab43ef2588b555019
p-koskey <patiencekoskey@gmail.com>
2020-11-02 12:24:48 +0300

    create path for profile and edit profile

--- hood/urls.py
+++ hood/urls.py
@@ -6,4 +6,7 @@ urlpatterns=[
     path('',views.index,name = 'index'),
     path('register/', views.register, name='register'),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('profile/<username>/edit', views.edit_profile, name='edit'),
+    
 ]
